5ceda2981979be74d56ac6de2154c856db2709a5
Sam Clements <sam.clements@datasift.com>
2014-03-14 17:47:24 +0000

    Show role descriptions and run lists

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="sous-chef",
-    version="0.6.0",
+    version="0.7.0",
     url="https://github.com/datasift/sous-chef",
 
     author="Sam Clements",
