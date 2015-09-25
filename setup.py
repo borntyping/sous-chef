4ee13ee5a9bad0c1c542ee7acf485cbb633e34e4
Sam Clements <sam@borntyping.co.uk>
2015-09-25 11:06:01 +0100

    Update version in __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='sous-chef',
-    version='2.6.1',
+    version='2.6.2',
     url='https://github.com/datasift/sous-chef',
 
     author="Sam Clements",
